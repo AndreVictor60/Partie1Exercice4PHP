87d2c2cf7c94e26cc336f0821b96fef381d9463d
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:02:07 +0100

    fini

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
-<?php
-  $string= 'Ceci est string. ';
-  $int = 17;
-  $float = 13.37;
-  $bool=true;
-  echo $string.'</br>';
-  echo 'Ceci est un int: '.$int.'</br>';
-  echo 'Ceci est un float: '.$float.'</br>';
-  echo 'Ceci est un bool: '.$bool.'</br>';
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 1 Exercice 4</title>
+  </head>
+  <body>
+    <?php
+      $string= 'Ceci est string. ';
+      $int = 17;
+      $float = 13.37;
+      $bool=true;
+      echo $string;
+      ?>
+      <br/>
+      <?php
+      echo 'Ceci est un int: ' . $int;
+      ?>
+      <br/>
+      <?php
+      echo 'Ceci est un float: ' . $float;
+      ?>
+      <br/>
+      <?php
+      echo 'Ceci est un bool: ' . $bool;
+     ?>
+  </body>
+</html>
